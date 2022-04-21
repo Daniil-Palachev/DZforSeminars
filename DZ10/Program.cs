16ca0b299117a720b0db4e9fd25017d2ad01a3c5
Daniil <danilaunited@gmail.com>
2022-04-21 19:42:43 +0300

    hw for seminar3

---
+++ DZ10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int sqrt = int.Parse(Console.ReadLine());
+int count= 0;
+
+while (count<=sqrt)
+{
+    double i = Math.Pow(count, 3);
+    count++;
+    Console.Write($"{i} ");
+}
