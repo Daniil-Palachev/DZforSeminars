b8eebf45f2314cc7ef406bb002ff1a2889c9311f
Daniil <danilaunited@gmail.com>
2022-04-21 19:44:57 +0300

    hw for seminar3

---
+++ DZ8/Program.cs
@@ -0,0 +1,19 @@
+﻿string num = Convert.ToString(new Random().Next(10000, 100000));
+Console.WriteLine(num);
+bool pol = true;
+for (int i = 0; i < num.Length / 2; i++)
+{
+    if (num[i] != num[num.Length - i-1]) 
+    {
+        pol = false;
+        break;
+    }
+}
+if (pol)
+{
+    Console.WriteLine("Число-полиндром");
+}
+else
+{
+    Console.WriteLine("Число не  полиндром");
+}
---
+++ DZ9/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите координаты");
+string[] cord =  Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+int x1 = int.Parse(cord[0]);
+int y1 = int.Parse(cord[1]);
+int z1 = int.Parse(cord[2]);
+int x2 = int.Parse(cord[3]);
+int y2 = int.Parse(cord[4]);
+int z2 = int.Parse(cord[5]);
+double lenth = Math.Pow(x2-x1,2);
+double high = Math.Pow(y2-y1,2);
+double width = Math.Pow(z2-z1,2);
+double sqrt = Math.Sqrt(lenth + high + width);
+
+Console.WriteLine($"{sqrt:f2}");
