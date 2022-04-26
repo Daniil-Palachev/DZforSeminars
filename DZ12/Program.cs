cfe2cb9d94ea7436015acdd441d2ce3cdd085c14
Daniil <danilaunited@gmail.com>
2022-04-26 21:23:46 +0300

    hw for seminar4

---
+++ DZ12/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+using System;
+Console.Clear();
+Console.WriteLine("Введите число");
+int A = int.Parse(Console.ReadLine());
+
+
+/*int Res(int num)
+{
+   if (num < 10)
+       return num;
+   int del = num % 10;
+   int nextdel = num / 10;
+   return del + Res(nextdel);
+}
+Console.WriteLine(Res(A));*/
+
+int Res(int num)
+{   
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num = num/10;
+    }
+    return sum;
+}
+Console.WriteLine(Res(A));
