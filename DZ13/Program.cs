a897214ffbb627c85e7695b83076f28089f4c172
Daniil <danilaunited@gmail.com>
2022-04-27 21:05:25 +0300

    hw for seminar4

--- DZ13/Program.cs
+++ DZ13/Program.cs
@@ -1,3 +1,13 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 //1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 //6, 1, 33 -> [6, 1, 33]
+
+using System;
+Console.Clear();
+Console.WriteLine(" Массив: ");
+int [] array = new int[8];
+for (int i=0; i<array.Length; i++)
+{
+    array[i]= new Random().Next(0,99);
+    Console.WriteLine(array[i] + " "+ " ->["+ array[i] + "]");
+}
