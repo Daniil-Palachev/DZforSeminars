98190a65bffac5ec81e3d69a4bfd3c6d5d7ef190
Daniil <danilaunited@gmail.com>
2022-05-15 14:42:14 +0300

    hw for seminar7

---
+++ DZ19/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+Console.Clear();
+
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+
+Console.WriteLine($"m = {m}, n = {n}");
+
+double [,] array= new double [m,n];
+
+Print (Getarray(array));
+
+double [,] Getarray(double [,] array)
+{
+    Random r=new Random();
+    for (int x=0; x<array.GetLength(0);x++)
+    {
+        for (int y=0; y<array.GetLength(1);y++)
+        array [x,y]=Convert.ToDouble(r.Next(-50, 50))/10;  
+    }
+    return array;
+}
+ 
+ void Print (double [,] array)
+{
+    for (int x=0; x<array.GetLength(0);x++)
+    {
+        for (int y=0; y<array.GetLength(1);y++)
+        Console.Write(array[x,y]); 
+        Console.WriteLine();  
+    }
+    
+}
